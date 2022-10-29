85c3736e55bb8fc08804962d191ca827ece53a66
Juan Camejo <juancamejo@hotmail.com>
2022-10-28 22:30:08 -0300

    Proyecto

--- stock.js
+++ stock.js
@@ -1,6 +1,14 @@
 let stockProductos = [
     {id: 1, nombre: "Buzo", tipo: "buzo", cantidad: 1, desc: "Un buzo a tu estilo", precio: 3000, talle: "L", img: './Imagenes/buzo.jpg'},
     {id: 2, nombre: "Remera", tipo: "remera", cantidad: 1, desc: "Una remera con tu estilo", precio: 1500, talle: "L", img: './Imagenes/remera.webp'},
-    {id: 3, nombre: "Jean", tipo: "jean", cantidad: 1, desc: "Un buzo que re va con vos", precio: 5000, talle: "M", img: './Imagenes/jean.jpg'},
+    {id: 3, nombre: "Jean", tipo: "jean", cantidad: 1, desc: "Un jean que re va con vos", precio: 5000, talle: "M", img: './Imagenes/jean.jpg'},
     {id: 4, nombre: "Zapatillas", tipo: "Zapatillas", cantidad: 1, desc: "Te acompaña a todos lados", precio: 6000, talle: "S", img: './Imagenes/calzadodeportivo.jpg'},
+    {id: 5, nombre: "Calzado femenino", tipo: "Calzado femenino", cantidad: 1, desc: "Te acompaña a todos lados", precio: 3000, talle: "S", img: './Imagenes/calzadofemenino.jpg'},
+    {id: 6, nombre: "Campera femenina", tipo: "Campera femenina", cantidad: 1, desc: "Acompañando tu estilo", precio: 3500, talle: "S", img: './Imagenes/camperafemenina.jpg'},
+    {id: 7, nombre: "Remera bicolor", tipo: "Remera bicolor", cantidad: 1, desc: "Destilando estilo", precio: 2000, talle: "M", img: './Imagenes/remeradehombre.jpg'},
+    {id: 8, nombre: "Equipo femenino completo", tipo: "Equipo femenino completo", cantidad: 1, desc: "Adaptado a tu estilo", precio: 6500, talle: "S", img: './Imagenes/equipofemeninocompleto.jpg'},
+    {id: 9, nombre: "Equipo deportivo femenino", tipo: "Equipo deportivo femenino ", cantidad: 1, desc: "Adaptado a tu estilo", precio: 3500, talle: "S", img: './Imagenes/equipodeportivofemenino.jpg'},
+    {id: 10, nombre: "Campera femenina gris", tipo: "Campera femenina", cantidad: 1, desc: "Te acompaña a todos lados", precio: 2500, talle: "M", img: './Imagenes/camperafemeninagris.jpg'},
+    {id: 11, nombre: "Campera femenina neopreno", tipo: "Campera", cantidad: 1, desc: "Te acompaña a todos lados", precio: 3600, talle: "M", img: './Imagenes/camperafemeninaneopreno.jpg'},
+    {id: 12, nombre: "Pantalon deportivo hombre", tipo: "Pantalon deportvo", cantidad: 1, desc: "Todo en deporte", precio: 2500, talle: "M", img: './Imagenes/pantalonhombre.jpg'},
 ]
